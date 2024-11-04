d36c3b0e9b33e14cfb404d8b471070468a75fe76
https://github.com/Shero48/Quiz.git <sunils42212@gmail.com>
2024-11-04 14:38:39 +0700

    new work

--- src/quiz.js
+++ src/quiz.js
@@ -9,8 +9,8 @@ const Quiz = () => {
   const [timer, setTimer] = useState(60);
   const [minute,setminute]=useState(14);
   const [show, setshow] = useState(false);
-  const [store, setStore] = useState([]);
-  const [load, setload] = useState(false);
+  // const [store, setStore] = useState([]);
+  // const [load, setload] = useState(false);
   const [id, setid] = useState(1);
   
 //   console.log(question[current]);
@@ -161,7 +161,7 @@ let paper=()=>{
       paper()
       //console.log(current);
     }
-    if (value == question[store[current]].answer) {
+    if (value == question[current].answer) {
       setScore((pre) => pre + 1);
       // setTimer(30);
       //console.log(current);
@@ -186,34 +186,33 @@ let paper=()=>{
 
     return () => clearInterval(time);
   }, [timer, show]);
-  let num;
-  useEffect(() => {
-    console.log("useEffect");
-    console.log("work");
-    const getUniqueRandomNumbers = (count, min, max) => {
-        const uniqueNumbers = new Set();
+  // useEffect(() => {
+  //   console.log("useEffect");
+  //   console.log("work");
+  //   const getUniqueRandomNumbers = (count, min, max) => {
+  //       const uniqueNumbers = new Set();
     
-        while (uniqueNumbers.size < count) {
-          const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
-          uniqueNumbers.add(randomNum);
-        }
-        const randomNumbersArray = Array.from(uniqueNumbers);
-        console.log("Generated random numbers:", randomNumbersArray);
-        setStore(randomNumbersArray);
-        return randomNumbersArray
-       };
-       return ()=>{
-        let value=getUniqueRandomNumbers(20,0,question.length-1)
-        console.log("value",value);
-       console.log("store",store);
-        // num=value;
-        // console.log("number",num);
-        // console.log(num[current]);
-        // console.log(num[1]);
-        // console.log(question[num[current]].question);
+  //       while (uniqueNumbers.size < count) {
+  //         const randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+  //         uniqueNumbers.add(randomNum);
+  //       }
+  //       const randomNumbersArray = Array.from(uniqueNumbers);
+  //       console.log("Generated random numbers:", randomNumbersArray);
+  //       setStore(randomNumbersArray);
+  //       return randomNumbersArray
+  //      };
+  //      return ()=>{
+  //       let value=getUniqueRandomNumbers(20,0,question.length-1)
+  //       console.log("value",value);
+  //      console.log("store",store);
+  //       // num=value;
+  //       // console.log("number",num);
+  //       // console.log(num[current]);
+  //       // console.log(num[1]);
+  //       // console.log(question[num[current]].question);
         
-       }
-  }, []); // Empty array ensures this runs once
+  //      }
+  // }, []); // Empty array ensures this runs once
     
   //console.log(num[current]);
   //console.log(value[current]);
@@ -232,11 +231,11 @@ let paper=()=>{
 
   return (
     <div className="quiz">
-      {!show && !load && store.length ? (
+      {!show  ? (
         <>
-          <h1 className="que">{id}.{question[store[current]].question}</h1>
+          <h1 className="que">{id}.{question[current].question}</h1>
           <div className="btns">
-            {question[store[current]].option.map((value, index) => (
+            {question[current].option.map((value, index) => (
               <button
                 className="btn"
                 key={index}
